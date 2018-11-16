9780c9a72dd74a6731564538271616ccace41017
edabruzzo <emmanuel.oliveira3@gmail.com>
2018-11-16 14:12:39 -0200

    Correções no código

---
+++ README.md
@@ -0,0 +1,15 @@
+CHALLENGE
+https://learn.freecodecamp.org/data-visualization/data-visualization-projects/visualize-data-with-a-bar-chart
+https://codepen.io/edabruzzo/pen/dQRQPZ
+
+PROJETO REFERENCIA
+https://codepen.io/freeCodeCamp/full/GrZVaM
+
+
+
+
+
+ESTUDO
+https://learn.freecodecamp.org/data-visualization/json-apis-and-ajax/access-the-json-data-from-an-api
+https://www.tutorialspoint.com/d3js/d3js_selections.htm
+https://www.npmjs.com/package/d3
--- app/scripts/barchart.js
+++ app/scripts/barchart.js
@@ -1,29 +1,26 @@
- 
+
 var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-document.addEventListener('DOMContentLoaded',function(){
-  document.getElementById('getMessage').onclick=function(){
-
-req=new XMLHttpRequest();
-req.open("GET",url,true);
-req.send();
-req.onload=function(){
-json=JSON.parse(req.responseText);
-document.getElementsByClassName('message')[0].innerHTML=JSON.stringify(json);
-};
-  };
-
+    req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.send();
+    req.onload = function () {
+      json = JSON.parse(req.responseText);
     
-    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
-    d3.select("body").selectAll("div")
-       .data(dataset)
-       .enter()
-       .append("div")
-       .attr("class", "bar")
-       // Add your code below this line
-       .style("height", (d) => (d + "px"));
-    // Add your code above this line
-    
- });
+  console.log(JSON.stringify(json));    
 
+  const dataset = json.data;
+     
+  console.log(dataset);
+
+  d3.select("body").selectAll("div")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", (d) => (d + "px"));
+
+  document.getElementsByClassName(`message`)[0].textContent = JSON.stringify(json);
+  
+  };      
