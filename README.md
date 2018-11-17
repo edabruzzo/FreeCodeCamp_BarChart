3a661a2fd9f00a569b4de8eeca29f0b4afb3fb7f
edabruzzo <emmanuel.oliveira3@gmail.com>
2018-11-17 17:56:06 -0200

    Construção do barchart completa, faltam agora as perfumarias

--- README.md
+++ README.md
@@ -5,11 +5,8 @@ https://codepen.io/edabruzzo/pen/dQRQPZ
 PROJETO REFERENCIA
 https://codepen.io/freeCodeCamp/full/GrZVaM
 
-
-
-
-
 ESTUDO
 https://learn.freecodecamp.org/data-visualization/json-apis-and-ajax/access-the-json-data-from-an-api
 https://www.tutorialspoint.com/d3js/d3js_selections.htm
 https://www.npmjs.com/package/d3
+https://www.youtube.com/watch?v=SDKDH7157IM
--- app/scripts/barchart.js
+++ app/scripts/barchart.js
@@ -1,26 +1,72 @@
 
 var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-    req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.send();
-    req.onload = function () {
-      json = JSON.parse(req.responseText);
-    
+req = new XMLHttpRequest();
+req.open("GET", url, true);
+req.send();
+req.onload = function () {
+  json = JSON.parse(req.responseText);
 
-  console.log(JSON.stringify(json));    
+  console.log(JSON.stringify(json));
 
   const dataset = json.data;
-     
-  console.log(dataset);
 
-  d3.select("body").selectAll("div")
+  console.log(dataset);
+  //document.write(dataset);
+
+  const w = 900;
+  const h = 460;
+  const scalefactor = 20;
+
+  var divmain = d3.select("body")
+    .append("div")
+    .attr("class", "main");
+
+  var container = divmain.append("div")
+    .attr("class", "container");
+
+  container.append("div").attr("id", "title");
+  document.getElementById("title").textContent = "Produto Interno Bruto dos EUA";
+
+  const svg = container
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  var legenda = svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -200)
+    .attr("y", 80)
+    .attr("id", "legendaY");
+
+  document.getElementById("legendaY").textContent = "Gross Domestic Product";
+
+
+  minimoAno = new Date(dataset[0][0].substring(0, 4));
+
+  maximoAno = new Date(dataset[dataset.length - 1][0].substring(0, 4));
+
+  const xScale = d3.scaleLinear()
+    .domain([minimoAno, maximoAno])
+    .range([0, w]);
+
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[1])])
+    .range([h, 0]);
+  
+
+  svg.selectAll("rect")
     .data(dataset)
     .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", (d) => (d + "px"));
+    .append("rect")
+    .attr("x", (d, i) => i * (w/dataset.length))
+    .attr("y", (d) => yScale(d[1]))
+    .attr("data-gdp", (d) => d[1])
+    .attr("data-date", (d) => d[0])
+    .attr("width", w/dataset.length)
+    .attr("height", (d,i) => h - yScale(d[1]))
+    .style("fill", "rgb(51 173 255)")
 
-  document.getElementsByClassName(`message`)[0].textContent = JSON.stringify(json);
-  
-  };      
+
+};
