d3187445b12ff55d4cd0a125e904506a85ac8cc2
edabruzzo <emmanuel.oliveira3@gmail.com>
2018-11-18 11:29:16 -0200

    Continuação do projeto

--- app/scripts/barchart.js
+++ app/scripts/barchart.js
@@ -42,10 +42,14 @@ req.onload = function () {
   document.getElementById("legendaY").textContent = "Gross Domestic Product";
 
 
-  minimoAno = new Date(dataset[0][0].substring(0, 4));
+//  minimoAno = new Date(dataset[0][0].substring(0, 4));
+  //maximoAno = new Date(dataset[dataset.length - 1][0].substring(0, 4));
+  
+  minimoAno = dataset[0][0].substring(0, 4);
+  maximoAno = dataset[dataset.length - 1][0].substring(0, 4);
 
-  maximoAno = new Date(dataset[dataset.length - 1][0].substring(0, 4));
 
+  
   const xScale = d3.scaleLinear()
     .domain([minimoAno, maximoAno])
     .range([0, w]);
@@ -54,19 +58,44 @@ req.onload = function () {
   const yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, (d) => d[1])])
     .range([h, 0]);
-  
+
+
+  const xAxis = d3.axisBottom(xScale);
+
+  svg.append("g")
+    .attr("id", "x-axis")
+    //.attr("transform", "translate(0, " + (h) + ")")
+    .attr("transform", "translate(60, 400)")
+    .attr("text-anchor", "middle")
+    .attr("font-size", 10)
+    .attr("font-family", "sans-serif")
+    .call(xAxis);
+
+  const yAxis = d3.axisLeft(yScale);
+
+
+  svg.append("g")
+    .attr("id", "y-axis")
+    //.attr("transform", "translate(" + h + ", 0)")
+    .attr("transform", "translate(60, 0)")
+    .attr("text-anchor", "end")
+    .attr("font-size", 10)
+    .attr("font-family", "sans-serif")
+    .call(yAxis);
+
+
 
   svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i * (w/dataset.length))
+    .attr("x", (d, i) => i * (w / dataset.length))
     .attr("y", (d) => yScale(d[1]))
     .attr("data-gdp", (d) => d[1])
     .attr("data-date", (d) => d[0])
-    .attr("width", w/dataset.length)
-    .attr("height", (d,i) => h - yScale(d[1]))
+    .attr("width", w / dataset.length)
+    .attr("height", (d, i) => h - yScale(d[1]))
     .style("fill", "rgb(51 173 255)")
 
 
-};
+};    
